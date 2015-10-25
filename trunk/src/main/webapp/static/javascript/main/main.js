ecb07454f8ca4fa583d398fde832156e945a31cf
lzh <499866197@qq.com>
2015-10-25 15:11:28 +0800

    更新菜单角色添加

--- trunk/src/main/webapp/static/javascript/main/main.js
+++ trunk/src/main/webapp/static/javascript/main/main.js
@@ -7,27 +7,3 @@
 jQuery(function($) {
     $('iframe_id').iframeAutoHeight({debug: true});
 });
-
-function subMenu(menuId){
-    $.ajax({
-        type: "post",
-        url: "sys/menuList",
-        data: {menuId:menuId},
-        dataType: "text",
-        success: function(data){
-            var json = $.parseJSON(data);
-            var html = "";
-            for(var i=0;i<json.length;i++){
-                html += "<ul class='submenu'><li>"+
-                            "<a href='"+json[i].url+"' class='dropdown-toggle'>"+
-                                "<i class='menu-icon fa fa-caret-right'></i>"+
-                                "<span class='menu-text'>"+json[i].name+"</span>"+
-                                "<b class='arrow fa fa-angle-down'></b>"+
-                            "</a>"+
-                        "</li></ul>";
-            }
-            $("#menu_"+menuId+" .submenu").html("");
-            $("#menu_"+menuId).append(html);
-        }
-    });
-}
