fe5d636c1754bcd9f7678df8cf7937b963a7005a
lzh <499866197@qq.com>
2015-10-31 20:08:31 +0800

    整理用户、角色代码

--- trunk/src/main/java/com/trunk/controller/SysController.java
+++ trunk/src/main/java/com/trunk/controller/SysController.java
@@ -8,6 +8,8 @@ import com.trunk.util.Common;
 import com.trunk.util.Pages;
 import com.trunk.util.ResultUtil;
 import com.trunk.util.TreeUtil;
+import com.trunk.util.xutil.StringUtils;
+import com.trunk.util.xutil.Validators;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,6 +18,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -94,9 +98,35 @@ public class SysController {
 
     //新增菜单
     @RequestMapping("/addMenu")
-    public String addMenu(Menu menu){
-        sysService.addMenu(menu);
-        return "sys/addMenu";
+    public void addMenu(HttpServletResponse response,Menu menu){
+        Map<String,Object> map = ResultUtil.result();
+        PrintWriter out = null;
+        int i = 0;
+        try{
+            out = response.getWriter();
+            if(Validators.isBlank(menu.getName())){
+                map.put("code",1);
+                map.put("msg","请输入菜单名称");
+            }else if(Validators.isBlank(menu.getResKey())){
+                map.put("code",2);
+                map.put("msg","请输入菜单标识");
+            }else if(Validators.isBlank(menu.getUrl())){
+                map.put("code",3);
+                map.put("msg","请输入菜单名称");
+            }else if(Validators.isBlank(menu.getDescription())){
+                map.put("code",4);
+                map.put("msg","请输入菜单描述");
+            }else{
+                sysService.addMenu(menu);
+                map.put("msg","菜单添加成功");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            logger.error(e);
+            map.put("code",-1);
+            map.put("msg","添加菜单失败,请联系管理员");
+        }
+        out.print("<script type=\"text/javascript\">parent.callback('"+ResultUtil.toJSON(map)+"')</script>");
     }
 
     //查询角色权限
--- trunk/src/main/webapp/static/javascript/sys/menu_add.js
+++ trunk/src/main/webapp/static/javascript/sys/menu_add.js
@@ -5,9 +5,76 @@
  * @version v1.0
  */
 $(function(){
-    $('#sub').on('click', function() {
-        $("#form").submit();
+
+    $("#form").bootstrapValidator({
+        message: '这个值无效',
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        live:'enabled',
+        excluded:[':disabled'],
+        fields: {
+            name:{
+                message: '菜单名称',
+                validators: {
+                    notEmpty: {
+                        message: '菜单名不能为空'
+                    },
+                    stringLength: {
+                        min: 2,
+                        max: 12,
+                        message: '菜单名长度为2-12'
+                    }
+                }
+            },
+            resKey:{
+                message: 'resKey无效',
+                validators: {
+                    notEmpty: {
+                        message: 'resKey不能为空'
+                    },
+                    stringLength: {
+                        min: 4,
+                        max: 12,
+                        message: 'resKey长度为4-12'
+                    }
+                }
+            },
+            url:{
+                message: 'url无效',
+                validators: {
+                    notEmpty: {
+                        message: 'url不能为空'
+                    },
+                    stringLength: {
+                        min: 1,
+                        max: 30,
+                        message: 'url长度为1-30'
+                    }
+                }
+            },
+            description:{
+                message: '描述无效',
+                validators: {
+                    notEmpty: {
+                        message: '描述不能为空'
+                    },
+                    stringLength: {
+                        min: 2,
+                        max: 12,
+                        message: '描述长度为2-12'
+                    }
+                }
+            }
+        }
     });
+
+    $("#reset").click(function(){
+        validate.data('bootstrapValidator').resetForm(true);
+    });
+
     $("#check").click(function(){
         if($('#check').is(':checked')){
             $("#ishide").val(1);
@@ -16,3 +83,24 @@ $(function(){
         }
     });
 });
+
+//回调函数
+function callback(data){
+    var obj = $.parseJSON(data);
+    if(obj.code == 0){
+        top.bootbox.alert({
+            size: 'small',
+            title:'提示:',
+            message: obj.msg,
+            callback: function(){
+                window.location.href = "sys/menuList";
+            }
+        });
+    }else{
+        top.bootbox.alert({
+            size: 'small',
+            title:'提示:',
+            message: obj.msg
+        });
+    }
+}
